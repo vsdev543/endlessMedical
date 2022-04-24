436d8726d9ded0a59d69d1af15cb7a05c5e57a8f
vsdev543 <ozi.cse20@gmail.com>
2022-04-24 22:31:26 +0530

    Added google translator

--- app/ui.R
+++ app/ui.R
@@ -176,6 +176,15 @@ shinyUI(
 
       gtag('config', 'UA-151880879-2');
       </script>
+      
+      
+      <script type='text/javascript'>
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script>
+
+<script type='text/javascript' src='//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'></script>
         "
       ),
       
@@ -434,7 +443,7 @@ background-attachment: fixed;
           ")
       )
     ),
-    
+    tags$div(id="google_translate_element"),
     conditionalPanel(
       condition = "$('html').hasClass('shiny-busy')",
       absolutePanel(
